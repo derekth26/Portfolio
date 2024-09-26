5caa6296955e1cea7d74a4de6de603171637cbe9
derekth26 <112732676+derekth26@users.noreply.github.com>
2024-09-26 14:28:47 -0600

    Add files via upload

---
+++ ChefAnt Landing Page/js/main.js
@@ -0,0 +1,96 @@
+
+// Navbar
+const menuToggle = document.getElementById('menu__toggle');
+const menuButton = document.querySelector('.menu__btn');
+const menuBox = document.querySelector('.menu__box');
+
+menuButton.addEventListener('click', (e) => {
+    console.log('Menu button clicked');
+    
+    // Toggle the checkbox state
+    menuToggle.checked = !menuToggle.checked;
+
+    // Apply the 'show-nav' class to the menu box based on the checkbox state
+    if (menuToggle.checked) {
+        menuBox.classList.add('show-nav');
+        console.log('Menu is shown');
+    } else {
+        menuBox.classList.remove('show-nav');
+        console.log('Menu is hidden');
+    }
+    
+    e.preventDefault();
+});
+
+
+
+
+//Card carousel
+  
+
+const prev = document.querySelector("#prev");
+const next = document.querySelector("#next");
+
+let carouselVp = document.querySelector("#carousel-vp");
+
+let cCarouselInner = document.querySelector("#cCarousel-inner");
+let carouselInnerWidth = cCarouselInner.getBoundingClientRect().width;
+
+let leftValue = 0;
+
+// Variable used to set the carousel movement value (card's width + gap)
+const totalMovementSize =
+  parseFloat(
+    document.querySelector(".cCarousel-item").getBoundingClientRect().width,
+    10
+  ) +
+  parseFloat(
+    window.getComputedStyle(cCarouselInner).getPropertyValue("gap"),
+    10
+  );
+
+prev.addEventListener("click", () => {
+  if (!leftValue == 0) {
+    leftValue -= -totalMovementSize;
+    cCarouselInner.style.left = leftValue + "px";
+  }
+});
+
+next.addEventListener("click", () => {
+  const carouselVpWidth = carouselVp.getBoundingClientRect().width;
+  if (carouselInnerWidth - Math.abs(leftValue) > carouselVpWidth) {
+    leftValue -= totalMovementSize;
+    cCarouselInner.style.left = leftValue + "px";
+  }
+});
+
+const mediaQuery510 = window.matchMedia("(max-width: 510px)");
+const mediaQuery770 = window.matchMedia("(max-width: 770px)");
+
+mediaQuery510.addEventListener("change", mediaManagement);
+mediaQuery770.addEventListener("change", mediaManagement);
+
+let oldViewportWidth = window.innerWidth;
+
+function mediaManagement() {
+  const newViewportWidth = window.innerWidth;
+
+  if (leftValue <= -totalMovementSize && oldViewportWidth < newViewportWidth) {
+    leftValue += totalMovementSize;
+    cCarouselInner.style.left = leftValue + "px";
+    oldViewportWidth = newViewportWidth;
+  } else if (
+    leftValue <= -totalMovementSize &&
+    oldViewportWidth > newViewportWidth
+  ) {
+    leftValue -= totalMovementSize;
+    cCarouselInner.style.left = leftValue + "px";
+    oldViewportWidth = newViewportWidth;
+  }
+}
+
+
+
+
+
+
