b4a9820eaa39e142c2f64203136dade7ec01f073
derekth26 <112732676+derekth26@users.noreply.github.com>
2024-09-26 14:30:50 -0600

    Add files via upload

---
+++ parallax-scroll-complete/js/main.js
@@ -0,0 +1,8 @@
+// Hamburger Menu (Show / Hide)
+
+// Because this is the only JavaScript we'll use for the course, we are 
+// keeping it as simple as possible (and not defining any constants or variables).
+
+// document.querySelector('.menu-controls').addEventListener('click', () => {
+//     document.querySelector('body').classList.toggle('show-menu');
+// });
