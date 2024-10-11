743f8608a776570e549e66f853ff8ec846b01a29
derekth26 <112732676+derekth26@users.noreply.github.com>
2024-10-11 17:53:38 +0000

    Oct 11, 2024, 11:53 AM

--- Derek Ho Portfolio/js/main.js
+++ Derek Ho Portfolio/js/main.js
@@ -41,28 +41,3 @@ $(window).on('load',function(){
 
 
 
-var swiper = new Swiper(".mySwiper", {
-	effect: "coverflow",
-	centeredSlides: true,
-	autoplay: {
-		delay: 3000,
-		disableOnInteraction: false
-	},
-	loop: true,
-	slidesPerView: "2",
-	coverflowEffect: {
-		rotate: 0,
-		stretch: 30,
-		depth: 100,
-		modifier: 5,
-		slideShadows: true
-	},
-	navigation: {
-		prevEl: ".swiper-button-prev",
-		nextEl: ".swiper-button-next"
-	},
-	pagination: {
-		el: ".swiper-pagination",
-		clickable: false
-	}
-});
