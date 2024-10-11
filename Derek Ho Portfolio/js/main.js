195339f99c6868632e4ea9eb9e581d677c3a8dec
derekth26 <112732676+derekth26@users.noreply.github.com>
2024-10-11 18:16:49 +0000

    Oct 11, 2024, 12:16 PM

--- Derek Ho Portfolio/js/main.js
+++ Derek Ho Portfolio/js/main.js
@@ -41,3 +41,47 @@ $(window).on('load',function(){
 
 
 
+function Slider() {
+    const carouselSlides = document.querySelectorAll('.slide');
+    const btnPrev = document.querySelector('.prev');
+    const btnNext = document.querySelector('.next');
+    const dotsSlide = document.querySelector('.dots-container');
+    let currentSlide = 0;
+  
+    const activeDot = function (slide) {
+        document.querySelectorAll('.dot').forEach(dot => dot.classList.remove('active'));
+        document.querySelector(`.dot[data-slide="${slide}"]`).classList.add('active');
+    };
+    activeDot(currentSlide);
+
+    const changeSlide = function (slides) {
+        carouselSlides.forEach((slide, index) => (slide.style.transform = `translateX(${100 * (index - slides)}%)`));
+    };
+    changeSlide(currentSlide);
+
+    btnNext.addEventListener('click', function () {
+        currentSlide++; 
+        if (carouselSlides.length - 1 < currentSlide) {
+            currentSlide = 0;
+        };
+        changeSlide(currentSlide);
+        activeDot(currentSlide);
+});
+    btnPrev.addEventListener('click', function () {
+        currentSlide--;
+        if (0 >= currentSlide) {
+            currentSlide = 0;
+        }; 
+        changeSlide(currentSlide);
+        activeDot(currentSlide);
+    });
+
+    dotsSlide.addEventListener('click', function (e) {
+        if (e.target.classList.contains('dot')) {
+            const slide = e.target.dataset.slide;
+            changeSlide(slide);
+            activeDot(slide);
+        }
+    });
+  };
+Slider();
